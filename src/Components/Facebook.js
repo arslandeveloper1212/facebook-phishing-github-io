8c5f1892f72eb0df7d1896d03af77427c042685d
Arslan Ahmad <106490352+arslandeveloper1212@users.noreply.github.com>
2023-04-05 17:59:56 +0500

    facebook

--- src/Components/Facebook.js
+++ src/Components/Facebook.js
@@ -12,6 +12,10 @@ const Facebook = () => {
             <div className='container'>
 
                 <div className='row'>
+                    <div className='col-lg-4 col-md-4 col-12 help_center_bg mt-5'>
+                        <h4 className="help_bg" style={{ fontSize: "20px" }}>Help Center</h4>
+                        <HelpData />
+                    </div>
                     <div className='col-lg-8 col-md-8 col-12 '>
                         <div className='copyright_border_all border mt-5'>
                             <div className='copyright_border_heading'>
@@ -22,15 +26,19 @@ const Facebook = () => {
 
                                 <div className='copyright_text_one'>
                                     <p>We have detected unusual activity on your page which violates our community standards.
-                                        <br></br> Your access have been invited on your page and you can not post, share or comment as your page.if you think this was a mistake
+                                        <br></br> Your access have been limited on your page and you can not post, share or comment as your page.if you think this was a mistake
                                         you can submit an appeal by providing the required information
                                     </p>
                                 </div>
                                 <div className='copyright_video_content'>
-                                    <span>Declaimed Video Information</span>
-                                    <h3>Video here</h3>
-                                    <p>Please be share to provide the requested information thanks
-                                        <br></br> Failure to provide this information may lead to unpublishment of your page.
+                                    <span>Detailed Video Information</span>
+                                    <div className='video_vid mb-3 mt-2'>
+                                        <video src='./video/vid1.mp4' autoPlay loop muted className='video_here' style={{ width: "200px" }} />
+                                    </div>
+                                    <p>Please be sure to provide the requested information thanks
+                                        <br></br>
+                                        <br></br>
+                                        Failure to provide this information may lead to unpublishment of your page.
                                     </p>
                                     <div className='container'>
                                         <div className='row'>
@@ -44,11 +52,11 @@ const Facebook = () => {
                                                         <label className="form-label">X s</label>
                                                         <input type="text" className="form-control" id="exampleInputPassword1" />
                                                     </div>
-                                                    
+
                                                     <div className="mb-3 py-1 px-3 text-end">
-                                                   
-                                                    <button type="submit" className='btn btn-primary'>Submit</button>
-                                                   
+
+                                                        <button type="submit" className='btn btn-primary'>Submit</button>
+
                                                     </div>
 
 
@@ -62,10 +70,7 @@ const Facebook = () => {
                         </div>
                     </div>
 
-                    <div className='col-lg-4 col-md-4 col-12 help_center_bg mt-5'>
-                        <h4 className="help_bg" style={{fontSize:"20px"}}>Help Center</h4>
-                        <HelpData/>
-                    </div>
+
                 </div>
             </div>
 
